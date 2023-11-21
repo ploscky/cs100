05ef56741e94d2bdf8e68ff6f2c92b2644b5c937
gababik <gbabi001@ucr.edu>
2023-11-20 21:18:23 -0800

    Moved code into correct files to correspond with UML diagram.

---
+++ header/game.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+
+void showMainMenu();
+void showHowToPlay();
+void showPlayScreen();
+void showGameOverScreen();
+void showGameLogScreen();
