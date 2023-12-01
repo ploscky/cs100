20fcb3c36127a0bbeea21af0f49f96fc23113f85
rzhou052 <rzhou052@ucr.edu>
2023-11-30 17:51:27 -0800

    Implemented bishop movement

--- header/game.h
+++ header/game.h
@@ -10,22 +10,22 @@ using namespace std;
 
 
 class Game {
-private:
-    Board *game;
-    vector<string> gameLog;
+    public:
+        Board *game;
+        vector<string> gameLog;
 
-public:
-    Game();
-    ~Game();
-    void mainMenu();
-    void printInstructions();
-    void play();
-    void printGameLog();
-    void saveGameLog(const string& fileName);
+    public:
+        Game();
+        ~Game();
+        void mainMenu();
+        void printInstructions();
+        void play();
+        void printGameLog();
+        void saveGameLog(const string& fileName);
 
-public: // private: for testing purposes
-    bool isCheckmate();
-    bool isStalemate();
-    bool isLegalMove(bool, string); // (white's turn, input)
-    bool inCheck();
+    public: // private: for testing purposes
+        bool isCheckmate();
+        bool isStalemate();
+        bool isLegalMove(bool, string); // (white's turn, input)
+        bool inCheck();
 };
