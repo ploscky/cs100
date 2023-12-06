f4d5030649cc166656c781f929a54333fd3eb7ac
Richard <114968899+rzhou052@users.noreply.github.com>
2023-12-05 18:22:58 -0800

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,3 @@
-#include "../header/board.h"
 #include "../header/game.h"
 #include <iostream>
 using namespace std;
@@ -6,11 +5,5 @@ using namespace std;
 int main() {
     Game g; 
 
-    cout << g.game->getLegalMoves(6,5) << endl;
-
-    
-    Board board1;
-    //board1.movePiece("e2", "e3");
-    board1.print();
     return 0;
 }
